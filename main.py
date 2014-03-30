d1e8f0caa13d874fa68a46b6fb7ed5cf9150763c
lucas amodeo newman <lucasamodeonewman@Lucas-MacBook-Pro.local>
2014-03-30 16:27:31 -0400

    blarg!

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, Markup
 
 app = Flask(__name__)
 
-@app.route('/url')
+@app.route('/url/')
 def url_shortener():
     return render_template('index.html')
+
+@app.route('/form_handler/', methods = ['GET', 'POST'])
+def form_handler():
+
+    return Markup('%s') % request.form['url'] # request.args.get('url')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
