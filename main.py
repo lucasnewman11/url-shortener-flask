79005075854bc39368502d25d9c793c8fe48ff03
Rose Ames <rose@happyspork.com>
2014-03-30 18:00:03 -0400

    stub out redirect function

--- main.py
+++ main.py
@@ -12,17 +12,22 @@ def url_shortener():
 
 @app.route('/form_handler/', methods = ['GET', 'POST'])
 def form_handler():
-    #set_trace()
     url = request.form['url']
-    filename = hashlib.sha1(url.encode('ascii')).hexdigest()
+    filename = hashlib.sha1(url.encode('ascii')).hexdigest()[:10]
     f = open(path + filename, 'w')
     f.write(url)
+    f.close()
     return Markup('To link to %s, use %s/redirect/%s') % (url, name, filename)
 
 @app.route('/redirect/<key>')
-def redirect():
-    pass
-    # look for file named key & redirect to the url in it
+def redirect(key):
+    f = open(path + key, 'r')
+    if f:
+        url = f.read()
+        return Markup('%s redirects to %s') % (key, url)
+        f.close()
+    else:
+        return Markup('%s not found') % key
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
